70a10adfde5e219d2663f89289274fd2635eeb31
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-03-02 09:26:30 -0800

    implemented action cable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
+    validates_presence_of :bio
+    validates_presence_of :full_name
     has_many :reservations
     has_many :attends
     has_many :restaurants, through: :reservations
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :attends
   resources :events
   resources :restaurants
+  resources :conversations, only: [:index, :create]
+  resources :messages, only: [:create]
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
